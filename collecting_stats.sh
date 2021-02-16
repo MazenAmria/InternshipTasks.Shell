b55c012d93adf1981ca1df022ad922adb193e2b0
MazenAmria <69749730+MazenAmria@users.noreply.github.com>
2021-02-16 12:34:01 +0200

    Create collecting_stats.sh

---
+++ collecting_stats.sh
@@ -0,0 +1,3 @@
+df --output=source,avail,used,pcent | tail -n +2 > disk.$(date "+%Y.%m.%d-%H.%M.%S")
+df --output=source,avail,used,pcent | tail -n +2 > disk.$(date "+%Y.%m.%d-%H.%M.%S")
+df --output=source,avail,used,pcent | tail -n +2 > disk.$(date "+%Y.%m.%d-%H.%M.%S")

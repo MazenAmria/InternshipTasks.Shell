e4966713efcbcbf3fdd2462fdbbad061bf598604
MazenAmria <69749730+MazenAmria@users.noreply.github.com>
2021-02-16 21:24:33 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,3 +5,9 @@ systemctl enable httpd
 systemctl start crond
 systemctl enable crond
 
+mkdir stats
+
+crontab -l > temp_file
+echo "* 1 * * * ./collecting_stats.sh" >> temp_file
+echo "* 1 * * * ./calc_avgs.sh" >> temp_file
+crontab temp_file

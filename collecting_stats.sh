5e32969451e6515062fa8167cf1d7c2d81726d57
MazenAmria <69749730+MazenAmria@users.noreply.github.com>
2021-03-14 14:47:50 +0200

    Update collecting_stats.sh

--- collecting_stats.sh
+++ collecting_stats.sh
@@ -1,3 +1,13 @@
-df --output=source,avail,used,pcent | sed -n 's/ */ /gp' > ~/stats/disk.$(date "+%Y.%m.%d-%H.%M.%S")
-iostat -c | sed -n 's/ */ /gp' | tail -n +2 > ~/stats/cpu.$(date "+%Y.%m.%d-%H.%M.%S")
-free | sed -n 's/ */ /gp' | cut -d' ' -f1,3,4 > ~/stats/memory.$(date "+%Y.%m.%d-%H.%M.%S")
+#!/bin/bash
+
+# record the timestamp
+timestamp=$(date "+%Y.%m.%d-%H.%M.%S")
+
+# collect the disk info
+df --output=source,avail,used,pcent > ~/stats/disk.$timestamp
+
+# collect the cpu info
+iostat -c | tail -n 2 > ~/stats/cpu.$timestamp
+
+# collect the memory info (fields: used [3], free [4])
+free | sed -n 's/ */ /gp' | cut -d' ' -f1,3,4 > ~/stats/memory.$timestamp

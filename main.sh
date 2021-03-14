c1f9a3868c1b9225ba09e2538d313f106d96ac17
MazenAmria <69749730+MazenAmria@users.noreply.github.com>
2021-03-14 14:42:47 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,13 +1,29 @@
+#!/bin/bash
+
+# install apache server and start it
 yum install -y httpd
 systemctl start httpd
-systemctl enable httpd
 
+# start cron service
 systemctl start crond
-systemctl enable crond
 
+# create a directory for the collected stats
 mkdir ~/stats/
 
-crontab -l > ~/temp_file
-echo "* 1 * * * ~/collecting_stats.sh" >> ~/temp_file
-echo "* 1 * * * ~/calc_avgs.sh" >> ~/temp_file
-crontab ~/temp_file
+# temp file to deal with the crontab
+temp_file=~/temp.$(date "+%Y.%m.%d-%H.%M.%S")
+
+# dump the crontab to the temp file
+crontab -l > $temp_file
+
+# add the cronjobs to the temp file
+# job 1: collect the stats
+echo "* 1 * * * ~/collecting_stats.sh" >> $temp_file
+# job 2: calculate the averages and modify the html
+echo "* 1 * * * ~/calc_avgs.sh" >> $temp_file
+
+# reload the crontab with the edited temp file
+crontab $temp_file
+
+# remove the tempfile
+rm -f $temp_file

d88336516aa24af32d432ce81c2d6f8d8b7bbd52
MazenAmria <69749730+MazenAmria@users.noreply.github.com>
2021-03-17 21:37:14 +0200

    Update main.sh

--- root/main.sh
+++ root/main.sh
@@ -43,13 +43,13 @@ crontab -l > $temp_file
 # job 1: collect the stats
 chmod +x ~/collecting_stats.sh
 sed -i "/^.*~\/collecting_stats.sh$/d" $temp_file
-echo "* * * * * ~/collecting_stats.sh" >> $temp_file
+echo "* 1 * * * ~/collecting_stats.sh" >> $temp_file
 
 # job 2: calculate the averages and modify the html
 chmod +x ~/calc_avgs.sh
 chmod +x ~/calc_avgs.awk
 sed -i "/^.*~\/calc_avgs.sh$/d" $temp_file
-echo "* * * * * ~/calc_avgs.sh" >> $temp_file
+echo "* 1 * * * ~/calc_avgs.sh" >> $temp_file
 
 # reload the crontab with the edited temp file
 crontab $temp_file

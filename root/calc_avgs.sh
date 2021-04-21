65a65efad17950a852d1753e9278b60269f459f3
MazenAmria <t.mazenamria@gmail.com>
2021-04-21 12:26:37 +0300

    Fixed: only 24 records are used to calculate average, if records are less than 24 no average is calculated

--- root/calc_avgs.sh
+++ root/calc_avgs.sh
@@ -3,23 +3,47 @@
 # define CPU stat page
 cpu=/var/www/html/cpu/index.html
 
-# update the page
+# fill the header part
 cat header.html > $cpu
-~/calc_avgs.awk $(ls ~/stats/cpu.*) >> $cpu
+
+if [ $(ls ~/stats/cpu.* | wc -w) -lt 24 ]; then
+	echo "No enough stats." >> $cpu
+elif
+	# get the last 24 records and calculates the average
+	~/calc_avgs.awk $(ls -lt ~/stats/cpu.* | tail -n +2 | head -n 24 | awk '{print $9}') >> $cpu
+fi
+
+# fill the footer part
 cat footer.html >> $cpu
 
 # define Memory stat page
 mem=/var/www/html/memory/index.html
 
-# update the page
+# fill the header part
 cat header.html > $mem
-~/calc_avgs.awk $(ls ~/stats/memory.*) >> $mem
+
+if [ $(ls ~/stats/mem.* | wc -w) -lt 24 ]; then
+	echo "No enough stats." >> $mem
+elif
+	# get the last 24 records and calculates the average
+	~/calc_avgs.awk $(ls -lt ~/stats/mem.* | tail -n +2 | head -n 24 | awk '{print $9}') >> $mem
+fi
+
+# fill the footer part
 cat footer.html >> $mem
 
 # define Disk stat page
 disk=/var/www/html/disk/index.html
 
-# update the page
+# fill the header part
 cat header.html > $disk
-~/calc_avgs.awk $(ls ~/stats/disk.*) >> $disk
+
+if [ $(ls ~/stats/disk.* | wc -w) -lt 24 ]; then
+	echo "No enough stats." >> $disk
+elif
+	# get the last 24 records and calculates the average
+	~/calc_avgs.awk $(ls -lt ~/stats/disk.* | tail -n +2 | head -n 24 | awk '{print $9}') >> $disk
+fi
+
+# fill the footer part
 cat footer.html >> $disk
--- root/main.sh
+++ root/main.sh
@@ -13,23 +13,23 @@ yum install -y sysstat
 # create a directory for the collected stats
 stats=~/stats/
 if [ ! -d "$stats" ]; then
-    mkdir $stats
+	mkdir $stats
 fi
 
 # create directories for the html files
 cpu=/var/www/html/cpu
 if [ ! -d "$cpu" ]; then
-    mkdir $cpu
+	mkdir $cpu
 fi
 
 memory=/var/www/html/memory
 if [ ! -d "$memory" ]; then
-    mkdir $memory
+	mkdir $memory
 fi
 
 disk=/var/www/html/disk
 if [ ! -d "$disk" ]; then
-    mkdir $disk
+	mkdir $disk
 fi
 
 # temp file to deal with the crontab
@@ -43,13 +43,13 @@ crontab -l > $temp_file
 # job 1: collect the stats
 chmod +x ~/collecting_stats.sh
 sed -i "/^.*~\/collecting_stats.sh$/d" $temp_file
-echo "* 1 * * * ~/collecting_stats.sh" >> $temp_file
+echo "0 * * * * ~/collecting_stats.sh" >> $temp_file
 
 # job 2: calculate the averages and modify the html
 chmod +x ~/calc_avgs.sh
 chmod +x ~/calc_avgs.awk
 sed -i "/^.*~\/calc_avgs.sh$/d" $temp_file
-echo "* 1 * * * ~/calc_avgs.sh" >> $temp_file
+echo "0 * * * * ~/calc_avgs.sh" >> $temp_file
 
 # reload the crontab with the edited temp file
 crontab $temp_file
